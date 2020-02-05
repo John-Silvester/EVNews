2f1679a31afff47790a8b6d3713f2d9fc8662e12
John-Silvester <58811826+John-Silvester@users.noreply.github.com>
2020-02-05 13:44:52 +1000

    Combine article routes into single route function using route variable

--- EV_news.py
+++ EV_news.py
@@ -26,175 +26,34 @@ def home():
                            outlet_page="/home")
 
 
-@app.route("/electrek", methods=['GET', 'POST'])
-def electrek():
+@app.route("/site/<site_name>", methods=['GET', 'POST'])
+def all_articles(site_name):
     global snoa
-    source_file = "electrek_articles.csv"
     if request.method == 'POST':
         snoa = int(request.form['page_num'])
+    sitetitles_file = "sitetitles.csv"
+    df1 = pd.read_csv(sitetitles_file, encoding='utf-8')
+    df1 = df1[df1.site.str.contains(site_name, case=False)]
+    title = df1['title']
+    title = title.to_string(index=False).strip()
+    outlet_page = "/site/" + df1['site']
+    outlet_page = outlet_page.to_string(index=False).strip()
+    source_file = df1['site'] + '_articles.csv'
+    source_file = source_file.to_string(index=False).strip()
+
     posts = retrieve_posts(source_file)
     return render_template("EV_article.html",
                            posts=posts,
-                           title="Electrek",
-                           outlet_page="/electrek")
-
-
-@app.route("/insideevs", methods=['GET', 'POST'])
-def insideevs():
-    global snoa
-    source_file = "insideevs_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="insideevs",
-                           outlet_page="/insideevs")
-
-
-@app.route("/teslarati", methods=['GET', 'POST'])
-def teslarati():
-    global snoa
-    source_file = "teslarati_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="TESLARATI",
-                           outlet_page="/teslarati")
-
-
-@app.route("/greencarguide", methods=['GET', 'POST'])
-def greencarguide():
-    global snoa
-    source_file = "greencarguide_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="Green Car Guide",
-                           outlet_page="/greencarguide")
-
-
-@app.route("/cleantechnica", methods=['GET', 'POST'])
-def cleantechnica():
-    global snoa
-    source_file = "cleantechnica_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="Clean Technica",
-                           outlet_page="/cleantechnica")
-
-
-@app.route("/greencarreports", methods=['GET', 'POST'])
-def greencarreports():
-    global snoa
-    source_file = "greencarreports_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="Green Car Reports",
-                           outlet_page="/greencarreports")
-
-
-@app.route("/chargedevs", methods=['GET', 'POST'])
-def chargedevs():
-    global snoa
-    source_file = "chargedevs_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="Charged EVs",
-                           outlet_page="/chargedevs")
-
-
-@app.route("/reneweconomy", methods=['GET', 'POST'])
-def reneweconomy():
-    global snoa
-    source_file = "reneweconomy_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="Renew Economy",
-                           outlet_page="/reneweconomy")
-
-
-@app.route("/evobsession", methods=['GET', 'POST'])
-def evobsession():
-    global snoa
-    source_file = "evobsession_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="EV Obsession",
-                           outlet_page="/evobsession")
-
-
-@app.route("/autoblog", methods=['GET', 'POST'])
-def autoblog():
-    global snoa
-    source_file = "autoblog_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="autoblog - green",
-                           outlet_page="/autoblog")
-
-
-@app.route("/thevergecars", methods=['GET', 'POST'])
-def thevergecars():
-    global snoa
-    source_file = "thevergecars_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="The Verge - cars",
-                           outlet_page="/thevergecars")
-
-
-@app.route("/electrive", methods=['GET', 'POST'])
-def electrive():
-    global snoa
-    source_file = "electrive_articles.csv"
-    if request.method == 'POST':
-        snoa = int(request.form['page_num'])
-    posts = retrieve_posts(source_file)
-    return render_template("EV_article.html",
-                           posts=posts,
-                           title="electrive",
-                           outlet_page="/electrive")
-
-
-@app.route("/updatearticles")
-def updatearticles():
-    subprocess.call([sys.executable, './update_articles.py'])
-    return render_template("EV_home.html", title="Home")
+                           title=title,
+                           outlet_page=outlet_page)
 
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
     global snoa
     if request.method == 'POST':
-        source_search = request.form['outlet']
+        source_search = request.form['outlet'].replace("/site/", "")
         source_file = source_search + "_articles.csv"
-        source_file = source_file.replace("/", "")
         df1 = pd.read_csv(source_file, encoding='utf-8')
         search_text = request.form['search']
         df2 = df1[df1.short_description.str.contains(search_text, case=False)]
@@ -203,12 +62,30 @@ def search():
         return render_template('EV_article.html',
                                title='Search ' + source_search,
                                posts=posts,
-                               outlet_page="/" + source_search)
+                               outlet_page="/site/" + source_search)
+
+
+@app.route("/updatearticles")
+def updatearticles():
+    subprocess.call([sys.executable, './update_articles.py'])
+    return render_template("EV_home.html", title="Home")
 
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/ev_database", endpoint='ev_database')
+@app.route("/evdb", endpoint='evdb')
+def evdb():
+    if request.endpoint == 'ev_database':
+        web_address = "https://ev-database.org/"
+    else:
+        web_address = "https://evdb.io/ev?sort=1&carType=car&evType=EV"
+    return render_template('EV_DB.html',
+                           title='EV DB',
+                           web_address=web_address)
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,60 +1,20 @@
-from bs4 import BeautifulSoup
-import requests
-from dateutil.parser import parse
 import pandas as pd
-import json
 
-articles_file = 'autoblog_articles.csv'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+site_name = "teslarati"
+sitetitles_file = "sitetitles.csv"
+df1 = pd.read_csv(sitetitles_file, encoding='utf-8')
+df1 = df1[df1.site.str.contains(site_name, case=False)]
 
-source = requests.get('https://www.autoblog.com/green/pg-2/#', headers=headers).text
-
-soup = BeautifulSoup(source, 'lxml')
-
-articles = soup.find_all("div", "list_record")
-# print(article)
-
-storiesdf = []
-
-for article in articles:
-    if article.find('script', type="application/ld+json") is None:
-        continue
-    scriptjson = article.find('script', type="application/ld+json").text
-    scriptjson = json.loads(scriptjson)
-    # print(scriptjson)
-
-    article_title = scriptjson['headline']
-    article_date = scriptjson['datePublished']
-    article_date = parse(article_date)
-    article_link = scriptjson['url']
-    article_byline = scriptjson['author']['name']
-    article_image = scriptjson['image']['url']
-    *_, article_image = article_image.split('http')
-    article_image = "http" + article_image
-    article_body = scriptjson['alternativeHeadline']
-    weboutlet = scriptjson['publisher']['name']
-    article_image_alt = "image not found"
-
-    storiesdf.append((article_date, article_title, article_body, article_link, article_image,
-                      article_byline, article_image_alt, weboutlet))
-
-    print()
-
-    print(article_date)
-    print(article_title)
-    print(article_body)
-    print(article_link)
-    print(article_image)
-    print(article_byline)
-    print(article_image_alt)
-    print(weboutlet)
-
-
-# df = pd.DataFrame(storiesdf, columns=['date', 'title', 'short_description', 'article_link', 'image',
-#                                       'byline', 'alt', 'outlet'])
-# df.to_csv(articles_file, index=False, encoding='utf-8')
-
-
-
+print(df1)
+title = df1['title']
+title = title.to_string(index=False).strip()
+outlet_page = "/" + df1['site']
+outlet_page = outlet_page.to_string(index=False).strip()
+source_file = df1['site'] + '_articles.csv'
+source_file = source_file.to_string(index=False).strip()
+df2 = pd.read_csv(source_file)
+df2 = df2.head(5)
+print(title)
+print(outlet_page)
+print(source_file)
+print(df2)
